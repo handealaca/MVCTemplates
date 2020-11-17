5605d8323391a68a74da6630e306102d717d6367
Hande Alaca <alacaa.hande@gmail.com>
2020-11-17 17:19:35 +0300

    first commit

---
+++ BusinessCasualTemplate/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessCasualTemplate.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessCasualTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            AboutVM about = new AboutVM();
+            about.LowerTitle = "Strong Tea, Strong Roots";
+            about.UpperTitle = "About Our Cafe";
+            about.Description1 = "Founded in 1986 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.";
+            about.Description2 = "We guarantee that you will fall in lust with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.";
+
+            return View(about);
+        }
+    }
+}
---
+++ BusinessCasualTemplate/Models/AboutVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessCasualTemplate.Models
+{
+    public class AboutVM
+    {
+        public string LowerTitle { get; set; }
+        public string UpperTitle { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+
+
+    }
+}
