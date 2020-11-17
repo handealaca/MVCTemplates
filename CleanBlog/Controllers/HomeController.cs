950c427af01275ffe73b3a3d350945f370218dd2
Hande Alaca <alacaa.hande@gmail.com>
2020-11-17 15:48:19 +0300

    first commit

---
+++ CleanBlog/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CleanBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PostVM> posts = new List<PostVM>();
+
+            PostVM post1 = new PostVM();
+            post1.Title = "ıvır zıvır";
+            post1.UserName="hande";
+            post1.AddDate = "11 Kasım";
+
+            PostVM post2 = new PostVM();
+            post2.Title = "ıvır zıvır ıvır zıvır";
+            post2.UserName = "bilge";
+            post2.AddDate = "06 Kasım";
+
+            posts.Add(post1);
+            posts.Add(post2);
+
+
+            return View(posts);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
---
+++ CleanBlog/Models/PostVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanBlog.Models
+{
+    public class PostVM
+    {
+        public string Title { get; set; }
+        public string UserName { get; set; }
+        public string AddDate { get; set; }
+    }
+}
